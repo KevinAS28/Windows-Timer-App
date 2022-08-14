4d0fbfae87e22097e7a4e380b7e19acde1898c66
KevinAS28 <kevinagusto28@gmail.com>
2022-08-14 23:21:32 +0700

    installer and exe

--- main.py
+++ main.py
@@ -1,33 +1,122 @@
-import loginLapas, timer, lapas, time, json
 
-def kill_wa():
-    lapas.find_stop('WhatsApp.exe')
+import loginLapas, timer, lapas, time, json, os
+from datetime import date, datetime
+from tkinter import messagebox
+from threading import Thread
 
-def close_kill(win):
-    win.destroy()
-    kill_wa()
+MAGIC_EXIT = 'polsek123'
+
+running = True
+loginned = False
+keep_kill = True
 
 def config(key):
     conf = json.loads(open('config.json').read())
     return conf[key]
 
+def valid_json(json_file):
+    try:
+        json.loads(open(json_file, 'r').read())
+        return True
+    except (json.JSONDecodeError, FileNotFoundError) as error:    
+        return False
+
+def quota_names(name):
+
+    quota_file = 'quotas.json'
+    if (not os.path.isfile(quota_file)) or (not valid_json(quota_file)):
+        with open(quota_file, 'w+') as qw:
+            qw.write(json.dumps({
+                'id_last': dict()
+            }))
+        print('wrote new quotas file')
+    
+    quotas = None
+    with (open(quota_file, 'r') as qr):
+        quotas = json.loads(qr.read())
+    print('quotas:', quotas)
+
+    toreturn = None
+
+    now = datetime.now()
+    id_last = quotas['id_last']
+    if name in id_last:
+        last = id_last[name]
+        last_login = datetime.strptime(last, '%Y-%m-%d %H:%M:%S.%f')
+        seconds_from_last = (now-last_login).total_seconds()
+        delay_seconds = int(config('delay_jam'))*3600 + int(config('delay_menit'))*60 + int(config('delay_detik'))
+        if seconds_from_last > delay_seconds:
+            quotas['id_last'][name] = str(datetime.now())
+            # qw.write(json.dumps(quotas))
+            print('login again')
+            toreturn = True, 0
+        else:
+            toreturn = False, delay_seconds-seconds_from_last
+    else:
+        quotas['id_last'][name] = str(now)
+        # qw.write(json.dumps(quotas))
+        toreturn = True, 0
+        print('new')
+
+    with open(quota_file, 'w+') as qw:
+        qw.write(json.dumps(quotas))
+    return toreturn
+
+           
+
+def kill_wa():
+    lapas.find_stop('WhatsApp.exe')
+
+def close_kill(win):
+    global running, loginned, keep_kill
+    running = False
+    loginned = False
+    keep_kill = False
+    win.destroy()
+    kill_wa()
+    time.sleep(3)
+    
+
+
+
 def call_log(s0, s1):
     with open('call_logs', 'a+') as cl:
-        cl.write(f'Nama Tahanan: {s0}\nNomor sel: {s1}\nWaktu mulai: {str(time.ctime())}\nDurasi: {config("jam")} jam {config("menit")} menit {config("detik")} detik\n\n')
-
-kill_wa()
+        cl.write(f'Nama Tahanan: {s0}\nNomor sel: {s1}\nWaktu mulai: {str(time.ctime())}\nDurasi: {config("timer_jam")} jam {config("timer_menit")} menit {config("timer_detik")} detik\n\n')
 
 def pending_function(win):
     win.destroy()
     kill_wa()
 
 def login(page, inputs):
+    global loginned 
+    loginned = True
+    if inputs['nama']==MAGIC_EXIT and inputs['nomor_sel']==MAGIC_EXIT:
+        close_kill(page.window)
+
+    allowed, remining_seconds = quota_names(inputs['nomor_sel'])
+    if not allowed:
+        messagebox.showwarning(f"Jatah anda sudah habis", f'bisa dicoba kembali setelah {int(remining_seconds)} detik')
+        return 
+
     call_log(inputs['nama'], str(inputs['nomor_sel']))
 
     page.window.destroy()
     kill_wa()
     lapas.start_wa()
     time.sleep(5)
-    timer.startTimer(config('jam'), config('menit'), config('detik'), pending_function, close_kill)
+    timer.startTimer(config('timer_jam'), config('timer_menit'), config('timer_detik'), pending_function, lambda *x: None)
 
-loginLapas.page(login, close_kill)
+def keep_kill():
+    global keep_kill
+    while keep_kill:
+        kill_wa()
+        time.sleep(3)
+    
+
+if __name__=='__main__':
+    while running:
+        kill_wa()
+        Thread(target=keep_kill).start()
+        loginLapas.page(login, close_kill)
+        messagebox.showinfo('Sesi anda sudah berakhir', 'Jatah anda hari ini sudah habis')
+        loginned = False
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-from distutils.core import setup
-import py2exe, sys, os
+import sys
+from cx_Freeze import setup, Executable
 
-sys.argv.append('py2exe')
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"includes": ["tkinter", 'PIL', 'psutil', 'ctypes'], 'include_files': ('images', 'quotas.json', 'config.json'), 'include_msvcr': True}
 
-setup(windows=[{'script': 'filename.py'}], \
-            options={"py2exe": {"includes": ["decimal", "Tkinter", \
-            "tkFileDialog", "csv", "xml.dom.minidom", "os"], \
-            'bundle_files': 1, 'compressed': False}}, \
-            zipfile = None)
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+# setup(
+#     name = "simple_Tkinter",
+#     version = "0.1",
+#     description = "Sample cx_Freeze Tkinter script",
+#     options = {"build_exe": build_exe_options},
+#     executables = [Executable("the timer.py", base = base)])
+
+setup(  name = "lapas",
+        version = "0.1",
+        description = "lapas",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("main.py", base=base)],
+
+        )
